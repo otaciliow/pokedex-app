7d399d43077bdf2a4254fffce68f9b8f002f4137
Otacilio Lima <otacilio.dml@hotmail.com>
2022-02-11 13:52:46 -0300

    sprites e icones dos tipos completos, e alteração de cores dinâmica

--- src/App.js
+++ src/App.js
@@ -10,9 +10,30 @@ export default function App() {
 
   const pokemonList = [
     {id: 1, nome: 'Bulbasaur', tipo1: 'Grama', tipo2: 'Venenoso'},
-    {id: 2, nome: 'Squirtle', tipo1: 'Água', tipo2: ''},
-    {id: 3, nome: 'Charmander', tipo1: 'Fogo', tipo2: ''}
+    {id: 2, nome: 'Ivysaur', tipo1: 'Grama', tipo2: 'Venenoso'},
+    {id: 3, nome: 'Venusaur', tipo1: 'Grama', tipo2: 'Venenoso'},
+    {id: 7, nome: 'Squirtle', tipo1: 'Água', tipo2: ''},
+    {id: 8, nome: 'Wartortle', tipo1: 'Água', tipo2: ''},
+    {id: 9, nome: 'Blastoise', tipo1: 'Água', tipo2: ''},
+    {id: 4, nome: 'Charmander', tipo1: 'Fogo', tipo2: ''},
+    {id: 5, nome: 'Charmeleon', tipo1: 'Fogo', tipo2: ''},
+    {id: 6, nome: 'Charizard', tipo1: 'Fogo', tipo2: 'Voador'}
     ];
+
+    const accentsMap = new Map([
+      ["-", "\\s|\\.|_"],
+      ["a", "á|à|ã|â|ä"],
+      ["e", "é|è|ê|ë"],
+      ["i", "í|ì|î|ï"],
+      ["o", "ó|ò|ô|õ|ö"],
+      ["u", "ú|ù|û|ü"],
+      ["c", "ç"],
+      ["n", "ñ"]
+    ]);
+    
+  const reducer = (acc, [key]) => acc.replace(new RegExp(accentsMap.get(key), "gi"), key);
+    
+  const slugify = (text) => [...accentsMap].reduce(reducer, text.toLowerCase());
 
   return (
     <div className="main-wrapper">
@@ -25,13 +46,12 @@ export default function App() {
         <ul className="card-wrapper">
           {pokemonList.map((pokemonList) => {
             return (
-              <li>
-                <PokemonCard 
-                  key={pokemonList.id} 
+              <li key={pokemonList.id} className={slugify(pokemonList.tipo1)}>
+                <PokemonCard                   
                   pokeName={pokemonList.nome} 
                   pokeNumero={pokemonList.id} 
-                  pokeTipo1={pokemonList.tipo1} 
-                  pokeTipo2={pokemonList.tipo2} 
+                  pokeTipo1={slugify(pokemonList.tipo1)} 
+                  pokeTipo2={slugify(pokemonList.tipo2)} 
                 />
               </li>
             )
--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,5 +1,5 @@
 import {React} from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 export default function PokemonCard(props) {
     
@@ -9,16 +9,20 @@ export default function PokemonCard(props) {
     const pokeTipo2 = props.pokeTipo2;
 
     // Ajustar navegação!!
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
         return(            
-        <div className="poke-wrapper">
+        <div className={`poke-wrapper ${pokeTipo1}`}>
             <img src={`./assets/images/pokemons/${pokeNum}.gif`} alt={`Imagem de um ${pokemon}`} />
             <div className="pokeInfos">
                 <span>{pokemon}</span>
                 <div className="pokeTipos">
-                    <span>{pokeTipo1}</span>
-                    {pokeTipo2 ? (<span>{pokeTipo2}</span>) : (<></>)}
+                    <img src={`./assets/images/tipos/${pokeTipo1}.svg`} alt={`Icone de tipo ${pokeTipo1}`} className={pokeTipo1} />
+                    {pokeTipo2 ? (
+                        <img src={`./assets/images/tipos/${pokeTipo2}.svg`} alt={`Icone de tipo ${pokeTipo2}`} className={pokeTipo2} />
+                    ) : (
+                    <></>
+                    )}
                 </div>
             </div>
         </div>
